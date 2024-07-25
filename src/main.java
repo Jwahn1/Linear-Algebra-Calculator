abfd538325f17361b2dd27493ae51a9d08432b93
Jwahn1 <javierwith@gmail.com>
2024-07-25 00:34:34 -0300

    Created the Vector class which handles all functions relating to vectors and calculations between 1 or more vectors.
    The main class is a temporary testing class.

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Vector A = new Vector(1,2,3);
+        Vector B = new Vector(4,5,6);
+
+        A.directionRatio();
+    }
+}
